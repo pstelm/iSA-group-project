7415a558fbdeb9827c092ef98bb71257b0479a38
Paulina <paulina.bistula@gmail.com>
2023-06-23 00:46:15 +0200

    ref: Change Layout.css to CSS module, clean index

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import './Layout.css';
+import styles from './Layout.module.css';
 
 const Layout = () => {
 	return (
-		<div className="layout">
-			<div className="navbar">Navbar (logo + menu button)</div>
-			<div className="page_content">
+		<div className={styles.layout}>
+			<div className={styles.navbar}>Navbar (logo + menu button)</div>
+			<div className={styles.page_content}>
 				<h1>Main section</h1>
 			</div>
-			<div className="footer">Footer (secondary navigation?)</div>
+			<div className={styles.footer}>Footer (secondary navigation?)</div>
 		</div>
 	);
 };
