9a1c54e53391c918dc1ba923e65fe829960d0d72
Katarzyna Machaj <katarzyna.machaj.94@gmail.com>
2023-06-25 08:41:11 +0200

    Add firebaseErrors.js and change order of errors in Register.jsx

--- src/components/Auth/Register/Register.jsx
+++ src/components/Auth/Register/Register.jsx
@@ -1,109 +1,113 @@
-import styles from './Register.module.css';
-import useAuth from '../../../contexts/AuthContext';
-import { toast } from 'react-hot-toast';
-import { auth, db } from '../../../config/firebase';
-import { doc, setDoc } from 'firebase/firestore';
-import { useNavigate } from 'react-router-dom';
+import styles from "./Register.module.css";
+import useAuth from "../../../contexts/AuthContext";
+import { toast } from "react-hot-toast";
+import { auth, db } from "../../../config/firebase";
+import { doc, setDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
+import { firebaseErrors } from "../../../utils/firebaseErrors";
 
 const Register = () => {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const { register } = useAuth();
+  const { register } = useAuth();
 
-	const addUserToCollection = async (newUserData) => {
-		try {
-			if (auth.currentUser) {
-				const docRef = doc(db, 'Users', auth.currentUser.uid);
+  const addUserToCollection = async (newUserData) => {
+    try {
+      if (auth.currentUser) {
+        const docRef = doc(db, "Users", auth.currentUser.uid);
 
-				await setDoc(docRef, newUserData);
+        await setDoc(docRef, newUserData);
 
-				toast.success('Witaj w aplikacji!');
-				navigate('/');
-			}
-		} catch (error) {
-			toast.error('Wystąpił błąd: ' + error.message);
-		}
-	};
+        toast.success("Witaj w aplikacji!");
+        navigate("/");
+      }
+    } catch (error) {
+      //   toast.error(firebaseErrors[error.code]);
+      //   toast.error("Wystąpił błąd: " + error.message);
+      toast.error(firebaseErrors[error.code]);
+    }
+  };
 
-	const handleSubmit = async (formEvent) => {
-		formEvent.preventDefault();
+  const handleSubmit = async (formEvent) => {
+    formEvent.preventDefault();
 
-		try {
-			const email = formEvent.target?.email.value;
-			const password = formEvent.target?.password.value;
-			const password_confirm = formEvent.target?.password_confirm.value;
-			const firstName = formEvent.target?.firstName.value;
-			const lastName = formEvent.target?.lastName.value;
-			const sex = formEvent.target?.sexF.checked ? 'F' : 'M';
+    try {
+      const email = formEvent.target?.email.value;
+      const password = formEvent.target?.password.value;
+      const password_confirm = formEvent.target?.password_confirm.value;
+      const firstName = formEvent.target?.firstName.value;
+      const lastName = formEvent.target?.lastName.value;
+      const sex = formEvent.target?.sexF.checked ? "F" : "M";
 
-			if (email === '') {
-				throw new Error('Email nie może być pusty');
-			}
+      if (firstName === "") {
+        throw new Error("Imię nie może być puste");
+      }
 
-			if (password === '') {
-				throw new Error('Hasło nie może być puste');
-			}
+      if (lastName === "") {
+        throw new Error("Nazwisko nie może być puste");
+      }
 
-			if (password !== password_confirm) {
-				throw new Error('Hasła muszą być takie same');
-			}
+      if (email === "") {
+        throw new Error("Email nie może być pusty");
+      }
 
-			if (firstName === '') {
-				throw new Error('Imię nie może być puste');
-			}
+      if (password === "") {
+        throw new Error("Hasło nie może być puste");
+      }
 
-			if (lastName === '') {
-				throw new Error('Nazwisko nie może być puste');
-			}
+      if (password !== password_confirm) {
+        throw new Error("Hasła muszą być takie same");
+      }
 
-			await register(email, password);
+      await register(email, password);
 
-			const newUserData = {
-				email: email,
-				firstName: firstName,
-				lastName: lastName,
-				sex: sex,
-			};
+      const newUserData = {
+        email: email,
+        firstName: firstName,
+        lastName: lastName,
+        sex: sex,
+      };
 
-			addUserToCollection(newUserData);
+      addUserToCollection(newUserData);
 
-			formEvent.target.reset();
-		} catch (error) {
-			toast.error('Wystąpił błąd: ' + error.message);
-		}
-	};
+      formEvent.target.reset();
+    } catch (error) {
+      toast.error(firebaseErrors[error.code]);
+      toast.error("Wystąpił błąd: " + error.message);
+    }
+  };
 
-	return (
-		<div className={styles.container}>
-			<form onSubmit={handleSubmit} className={styles.form}>
-				<label htmlFor="text">Imię</label>
-				<input type="text" name="firstName" id="firstName" />
-				<label htmlFor="text">Nazwisko</label>
-				<input type="text" name="lastName" id="lastName" />
-				<label htmlFor="sexF">Kobieta</label>
-				<input type="radio" name="sex" id="sexF" defaultChecked={true} />
-				<label htmlFor="sexM">Mężczyzna</label>
-				<input type="radio" name="sex" id="sexM" />
-				<label htmlFor="email">Adres email</label>
-				<input type="email" name="email" id="email" />
-				<label htmlFor="password">Hasło</label>
-				<input
-					type="password"
-					name="password"
-					id="password"
-					pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?!.*[&%$]).{8,}$"
-					title="Twoje hasło musi zawierać 8 znaków, co najmniej 1 cyfrę, 1 znak specjalny, 1 dużą literę i 1 małą literę"
-				/>
-				<p>
-					Twoje hasło musi zawierać 8 znaków, co najmniej 1 cyfrę, 1 znak specjalny, 1 dużą i 1 małą
-					literę
-				</p>
-				<label htmlFor="password_confirm">Powtórz hasło</label>
-				<input type="password" name="password_confirm" id="password_confirm" />
-				<button type="submit">Zarejestruj się</button>
-			</form>
-		</div>
-	);
+  return (
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <label htmlFor="text">Imię</label>
+        <input type="text" name="firstName" id="firstName" />
+        <label htmlFor="text">Nazwisko</label>
+        <input type="text" name="lastName" id="lastName" />
+        <label htmlFor="sexF">Kobieta</label>
+        <input type="radio" name="sex" id="sexF" defaultChecked={true} />
+        <label htmlFor="sexM">Mężczyzna</label>
+        <input type="radio" name="sex" id="sexM" />
+        <label htmlFor="email">Adres email</label>
+        <input type="email" name="email" id="email" />
+        <label htmlFor="password">Hasło</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?!.*[&%$]).{8,}$"
+          title="Twoje hasło musi zawierać 8 znaków, co najmniej 1 cyfrę, 1 znak specjalny, 1 dużą literę i 1 małą literę"
+        />
+        <p>
+          Twoje hasło musi zawierać 8 znaków, co najmniej 1 cyfrę, 1 znak
+          specjalny, 1 dużą i 1 małą literę
+        </p>
+        <label htmlFor="password_confirm">Powtórz hasło</label>
+        <input type="password" name="password_confirm" id="password_confirm" />
+        <button type="submit">Zarejestruj się</button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
---
+++ src/utils/firebaseErrors.js
@@ -0,0 +1,8 @@
+export const firebaseErrors = {
+  "auth/user-not-found": "Podany użytkownik nie istnieje",
+  "auth/invalid-email": "Nieprawidłowy email",
+  "auth/weak-password":
+    "Hasło musi zawierać 8 znaków, co najmniej 1 cyfrę, 1 znak specjalny, 1 dużą literę i 1 małą literę",
+  "auth/email-already-in-use":
+    "Istnieje już konto zarejestrowane dla tego adresu e-mail",
+};
