2b322cd0ad17896f1c5dc5aac7a94780ac96c9fa
Katarzyna Machaj <katarzyna.machaj.94@gmail.com>
2023-06-25 09:43:27 +0200

    Correct Firebase errors

--- src/utils/firebaseErrors.js
+++ src/utils/firebaseErrors.js
@@ -1,6 +1,7 @@
 export const firebaseErrors = {
   "auth/user-not-found": "Podany użytkownik nie istnieje",
   "auth/wrong-password": "Błędne hasło",
+  "auth/too-many-requests": "Zbyt wiele prób błędnego logowania",
   "auth/invalid-email": "Nieprawidłowy email",
   "auth/weak-password":
     "Hasło musi zawierać 8 znaków, co najmniej 1 cyfrę, 1 znak specjalny, 1 dużą literę i 1 małą literę",
