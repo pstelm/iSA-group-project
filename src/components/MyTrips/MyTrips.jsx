7bf576aed992507864426851059bd0a2551f31d1
Paulina <paulina.bistula@gmail.com>
2023-06-28 17:49:32 +0200

    feat: Add MyTrips navigation with styling

--- src/components/MyTrips/MyTrips.jsx
+++ src/components/MyTrips/MyTrips.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import styles from './MyTrips.module.css';
+import { Link, Outlet } from 'react-router-dom';
 
 const MyTrips = () => {
-	return <div>Lista moich podróży - dołączonych i stworzonych</div>;
+	return (
+		<div className={styles.container}>
+			<h4>Moje podróże</h4>
+			<div className={styles.buttonBox}>
+				<Link to='ownedtrips' className={styles.myTripsLink}>
+					Moje
+				</Link>
+				<Link to='joinedtrips' className={styles.myTripsLink}>
+					Dołączone
+				</Link>
+			</div>
+			<Outlet />
+		</div>
+	);
 };
 
 export default MyTrips;
