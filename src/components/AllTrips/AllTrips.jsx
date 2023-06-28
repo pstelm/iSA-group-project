d2c5f19d8661156c061cb864774b9750926b1503
Paulina <paulina.bistula@gmail.com>
2023-06-29 00:03:54 +0200

    feat: Update AllTrips component with Trip componen

--- src/components/AllTrips/AllTrips.jsx
+++ src/components/AllTrips/AllTrips.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import styles from './AllTrips.module.css'
+import React, { useEffect, useState } from 'react';
+import styles from './AllTrips.module.css';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../../config/firebase';
+import useAuth from '../../contexts/AuthContext';
+import Trip from '../Trip/Trip';
 
 const AllTrips = () => {
-  return (
-    <div>Tutaj będzie widoczna lista wszystkich podróży</div>
-  )
-}
+	const { currentUser } = useAuth();
+	const allTripsCollectionRef = collection(db, 'Trips');
+	const [trips, setTrips] = useState([]);
 
-export default AllTrips
+	const getTrips = async () => {
+		try {
+			const tripsSnapshot = await getDocs(allTripsCollectionRef);
+			const tripsData = tripsSnapshot.docs.map((doc) => ({
+				id: doc.id,
+				...doc.data(),
+			}));
+			setTrips(tripsData);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		getTrips();
+		// console.log(trips);
+	}, []);
+
+	return (
+		<div className={styles.container}>
+			<h4>Podróżuj</h4>
+			<ul>
+				{trips ? trips.map((trip) => <Trip key={trip.id} {...trip} />) : null}
+			</ul>
+		</div>
+	);
+};
+
+export default AllTrips;
