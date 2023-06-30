96642993b7a08fb0f9527b3a419081bc1615e795
Paulina <paulina.bistula@gmail.com>
2023-06-30 16:19:51 +0200

    Change class p to .containter p in Register.module.css

--- src/components/Trip/TripFullPage/TripFullPage.jsx
+++ src/components/Trip/TripFullPage/TripFullPage.jsx
@@ -2,25 +2,56 @@ import React, { useEffect, useState } from 'react';
 import styles from './TripFullPage.module.css';
 import { useParams } from 'react-router-dom';
 import { getUserData } from '../../../utils/getUserData';
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, getDoc, getDocs } from 'firebase/firestore';
 import { db } from '../../../config/firebase';
 
 const TripFullPage = () => {
 	const { tripID } = useParams();
 
 	const [trip, setTrip] = useState();
+	const [participant, setParticipant] = useState();
 
 	const getTrip = async () => {
 		const tripRef = await doc(db, 'Trips', tripID);
 
 		return await getDoc(tripRef).then((tripSnap) => {
 			setTrip({ id: tripID, ...tripSnap.data() });
+			// console.log(tripSnap.data());
+			// getParticipants(tripSnap.data().participants);
+			// getParticipants();
 		});
 	};
 
+	const getParticipants = async () => {
+		console.log('Uczestnicy: ', trip.participants);
+		const participantsData = [];
+
+		trip.participants.forEach((item) => {
+			// console.log(item);
+			const partRef = doc(db, 'Users', item);
+			getDoc(partRef).then((partSnap) => {
+				const part = { id: item, ...partSnap.data() };
+				console.log(part);
+			});
+			// const part = { id: item, ...partSnap.data() };
+		});
+		// const participantsData = tripSnapshot.participants.map((person) => {
+		// 	console.log('Person: ', person);
+		// 	const docRef = doc(db, 'Users', person);
+		// 	const docData = getDoc(docRef);
+		// 	console.log(docData.data());
+		// 	// person = participant;
+		// });
+
+		// // console.log(participantsData);
+		return participantsData;
+	};
+
 	useEffect(() => {
-		getTrip();
-		console.log(trip);
+		// getTrip();
+		getTrip().then((tripSnapshot) => {
+			getParticipants();
+		});
 	}, []);
 
 	return (
@@ -39,8 +70,37 @@ const TripFullPage = () => {
 							/>
 							<p>{trip.endPlace}</p>
 						</div>
-						<p></p>
-						{/* <p>TripID: {tripID}</p> */}
+						<div className={styles.oneLine}>
+							<img
+								className={styles.icon}
+								src='/src/assets/icons/calendar-days-regular.svg'
+								alt=''
+							/>
+							<p>Termin podróży:</p>
+						</div>
+						<p>Początek: {trip.startDate.toDate().toLocaleDateString()}</p>
+						<p>Koniec: {trip.endDate.toDate().toLocaleDateString()}</p>
+						<button className={styles.addToTripBtn}>Dołącz do podróży</button>
+
+						<div className={styles.tagsBox}>
+							{trip.tags.map((tag) => {
+								return (
+									<span key={tag} className={styles.tag}>
+										{tag}
+									</span>
+								);
+							})}
+						</div>
+
+						<div className={styles.box}>
+							<h4 className={styles.sectionTitle}>Uczestnicy</h4>
+							<p>{trip.participants.join(', ')}</p>
+						</div>
+
+						<div className={styles.box}>
+							<h4 className={styles.sectionTitle}>Opis podróży</h4>
+							<p>{trip.info}</p>
+						</div>
 					</div>
 				</div>
 			) : null}
