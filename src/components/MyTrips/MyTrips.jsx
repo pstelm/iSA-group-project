51e3cd0d838baa00292563002561e5f3af831cb6
Katarzyna Machaj <katarzyna.machaj.94@gmail.com>
2023-07-01 13:01:18 +0200

    fix: adjust MyTrips component to new link in Home.jsx

--- src/components/MyTrips/MyTrips.jsx
+++ src/components/MyTrips/MyTrips.jsx
@@ -3,14 +3,21 @@ import styles from './MyTrips.module.css';
 import { Link, Outlet } from 'react-router-dom';
 
 const MyTrips = () => {
+	const OWNEDTRIPS = '/mytrips/ownedtrips';
+
 	return (
 		<div className={styles.container}>
 			<h4>Moje podróże</h4>
 			<div className={styles.buttonBox}>
-				<Link to='ownedtrips' className={styles.myTripsLink}>
+				<Link
+					to='ownedtrips'
+					className={`${styles.myTripsLink} ${
+						window.location.pathname === OWNEDTRIPS ? styles.myTripsLinkActive : ''
+					}`}
+				>
 					Moje
 				</Link>
-				<Link to='joinedtrips' className={styles.myTripsLink}>
+				<Link to='joinedtrips' className={`${styles.myTripsLink} `}>
 					Dołączone
 				</Link>
 			</div>
