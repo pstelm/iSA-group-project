fbe6bf137bdbccf34cde10edaf201e78d074715b
Paulina <paulina.bistula@gmail.com>
2023-07-01 13:37:44 +0200

    Add TripFullPage component final version

--- src/components/Trip/TripFullPage/TripFullPage.jsx
+++ src/components/Trip/TripFullPage/TripFullPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import styles from './TripFullPage.module.css';
 import { useParams } from 'react-router-dom';
 // import { getUserData } from '../../../utils/getUserData';
-import { doc, getDoc } from 'firebase/firestore';
+import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
 import { db } from '../../../config/firebase';
 import { toast } from 'react-hot-toast';
 
@@ -10,49 +10,61 @@ const TripFullPage = () => {
 	const { tripID } = useParams();
 
 	const [trip, setTrip] = useState();
-	const [users, serUsers] = useState();
-	const [participantsDataArr, setParticipantsDataArr] = useState();
+	const [users, setUsers] = useState();
+	const [participantsData, setParticipantsData] = useState();
 
-	const getTrip = async () => {
-		const tripRef = await doc(db, 'Trips', tripID);
+	const getUsers = async () => {
+		const usersRef = collection(db, 'Users');
 
-		return await getDoc(tripRef).then((tripSnap) => {
-			setTrip({
-				id: tripID,
-				...tripSnap.data(),
-			});
-			getParticipants(tripSnap.data().participants);
-		});
+		try {
+			const usersSnap = await getDocs(usersRef);
+			const usersFilteredData = usersSnap.docs.map((doc) => ({
+				id: doc.id,
+				...doc.data(),
+			}));
+			setUsers(usersFilteredData);
+			getTrip();
+		} catch (error) {
+			console.error(error);
+		}
 	};
 
-	const getParticipants = async (tripParticipantsArr) => {
-		const participantsDataResolved = await Promise.all(
-			tripParticipantsArr.map((item) => {
-				const partRef = doc(db, 'Users', item);
-				return getDoc(partRef).then((userSnapshot) => {
-					const userDataFiltered = {
-						id: item,
-						...userSnapshot.data(),
-					};
-					return userDataFiltered;
-				});
-			})
-		);
+	const getTrip = async () => {
+		const tripRef = doc(db, 'Trips', tripID);
 
-		setParticipantsDataArr(participantsDataResolved);
+		try {
+			const tripSnap = await getDoc(tripRef);
+			if (tripSnap.exists()) {
+				const tripFilteredData = tripSnap.data();
+				setTrip(tripFilteredData);
+			} else {
+				toast.error('Ta wycieczka już nie istnieje');
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	const getParticipants = () => {
+		const participantsFilteredArray = users.filter((doc) => {
+			return trip?.participants.some((partID) => doc.id.includes(partID));
+		});
+		setParticipantsData(participantsFilteredArray);
 	};
 
 	useEffect(() => {
-		getTrip();
-		// Wydaje mi się, że pomimo .then() getParticipans działo sie jeszcze przed rozwiązaniem getTrip()
-		// getTrip().then((tripSnap) => {
-		// 	getParticipants(tripSnap);
-		// });
+		getUsers();
 	}, []);
+
+	useEffect(() => {
+		if (users && trip) {
+			getParticipants();
+		}
+	}, [users, trip]);
 
 	return (
 		<>
-			{trip ? (
+			{participantsData ? (
 				<div className={styles.container}>
 					<h3>Podróże</h3>
 
@@ -76,8 +88,8 @@ const TripFullPage = () => {
 						</div>
 						<p>Początek: {trip.startDate.toDate().toLocaleDateString()}</p>
 						<p>Koniec: {trip.endDate.toDate().toLocaleDateString()}</p>
-						{participantsDataArr &&
-						participantsDataArr.length < trip.maxParticipantsCount ? (
+						{participantsData &&
+						participantsData.length < trip.maxParticipantsCount ? (
 							<button className={styles.addToTripBtn}>Dołącz do podróży</button>
 						) : (
 							<button className={styles.addToTripBtnDisabled} disabled>
@@ -100,13 +112,21 @@ const TripFullPage = () => {
 								<span>Uczestnicy</span>
 								<span>max. {trip.maxParticipantsCount}</span>
 							</h4>
-							{participantsDataArr
-								? participantsDataArr.map((participant) => {
-										return (
-											<p key={participant.id} className={styles.participant}>
-												{participant.firstName} {participant.lastName}
-											</p>
-										);
+							{participantsData
+								? participantsData.map((participant) => {
+										if (participant.id === trip.owner) {
+											return (
+												<p key={participant.id} className={styles.owner}>
+													{participant.firstName} {participant.lastName} - Organizator
+												</p>
+											);
+										} else {
+											return (
+												<p key={participant.id} className={styles.participant}>
+													{participant.firstName} {participant.lastName}
+												</p>
+											);
+										}
 								  })
 								: null}
 						</div>
@@ -117,7 +137,9 @@ const TripFullPage = () => {
 						</div>
 					</div>
 				</div>
-			) : null}
+			) : (
+				<p>Trwa ładowanie</p>
+			)}
 		</>
 	);
 };
