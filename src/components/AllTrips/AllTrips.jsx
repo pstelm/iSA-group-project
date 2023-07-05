99e480af591ac39fd4171db6fbfeda6ec589f217
Paulina <paulina.bistula@gmail.com>
2023-07-05 15:45:58 +0200

    Change AllTrips to show also trips created by currentUser

--- src/components/AllTrips/AllTrips.jsx
+++ src/components/AllTrips/AllTrips.jsx
@@ -2,22 +2,14 @@ import React, { useEffect, useState } from 'react';
 import styles from './AllTrips.module.css';
 import { collection, getDocs, query, where } from 'firebase/firestore';
 import { db } from '../../config/firebase';
-import useAuth from '../../contexts/AuthContext';
 import TripMini from '../Trip/TripMini/TripMini';
 
 const AllTrips = () => {
-	const { currentUser } = useAuth();
 	const [trips, setTrips] = useState([]);
-
-	// Pobieram referencję do wszystkich wycieczek, których aktualnie zalogowany user nie jest właścicielem
-	const filteredTripsCollectionRef = query(
-		collection(db, 'Trips'),
-		where('owner', '!=', currentUser.uid)
-	);
 
 	const getTrips = async () => {
 		try {
-			const tripsSnapshot = await getDocs(filteredTripsCollectionRef);
+			const tripsSnapshot = await getDocs(collection(db, 'Trips'));
 			const tripsData = tripsSnapshot.docs.map((doc) => ({
 				id: doc.id,
 				...doc.data(),
