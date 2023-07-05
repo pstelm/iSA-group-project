417e5b8944df8640b6b17c1dc31cc6255bc739b1
Paulina <paulina.bistula@gmail.com>
2023-07-05 20:30:36 +0200

    Add gray colors and try-catch to getUserData

--- src/utils/getUserData.js
+++ src/utils/getUserData.js
@@ -4,20 +4,20 @@ import { db } from '../config/firebase';
 export const getUserData = async (userId, setUser) => {
 	const userRef = await doc(db, 'Users', userId);
 
-	// try {
-	// 	const userData = await getDoc(userRef);
-	// 	if (userData.exists()) {
-	// 		const userDataFiltered = userData.data();
-	// 		setUser(userDataFiltered);
-	// 	}
-	// } catch (error) {
-	// 	console.log(error);
-	// }
+	try {
+		const userData = await getDoc(userRef);
+		if (userData.exists()) {
+			const userDataFiltered = userData.data();
+			setUser(userDataFiltered);
+		}
+	} catch (error) {
+		console.log(error);
+	}
 
-	return await getDoc(userRef).then((userSnapshot) => {
-		setUser({
-			id: userId,
-			...userSnapshot.data(),
-		});
-	});
+	// return await getDoc(userRef).then((userSnapshot) => {
+	// 	setUser({
+	// 		id: userId,
+	// 		...userSnapshot.data(),
+	// 	});
+	// });
 };
