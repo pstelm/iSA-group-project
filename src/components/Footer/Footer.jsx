c16169163729dfc83a75ffb21244dde3d90054ec
Mateusz Czekaj <mati.czekaj@gmail.com>
2023-07-08 10:35:10 +0200

    Add light and dark icons functionality in footer, correct svg account circle

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,54 +1,73 @@
 import React from 'react';
 import styles from './Footer.module.css';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Footer = () => {
+	const { pathname } = useLocation();
+
 	return (
 		<div className={styles.footer}>
-			<Link className={`{pathname === '/' ? styles.active : ''}`} to='/mytrips'>
+			<Link className={pathname === '/' ? styles.active : ''} to='/'>
 				<div className={styles.link}>
 					<img
 						className={styles.icon}
-						src='../../src/assets/icons/home-circle-dark.svg'
+						src={
+							pathname === '/'
+								? '../../src/assets/icons/home-circle-light.svg'
+								: '../../src/assets/icons/home-circle-dark.svg'
+						}
 						alt='Ikona skrótu do Home'
 					/>
 					<span>Home</span>
 				</div>
 			</Link>
 			<Link
-				className={`{pathname === '/alltrips' ? styles.active : ''}`}
-				to='/mytrips'
+				className={pathname === '/alltrips' ? styles.active : ''}
+				to='/alltrips'
 			>
 				<div className={styles.link}>
 					<img
 						className={styles.icon}
-						src='../../src/assets/icons/airplane-dark.svg'
+						src={
+							pathname === '/alltrips'
+								? '../../src/assets/icons/airplane-light.svg'
+								: '../../src/assets/icons/airplane-dark.svg'
+						}
 						alt='Ikona skrótu do Podróżuj'
 					/>
+
 					<span>Podróżuj</span>
 				</div>
 			</Link>
 			<Link
-				className={`{pathname === '/mytrips/ownedtrips' ? styles.active : ''}`}
+				className={pathname === '/mytrips/ownedtrips' ? styles.active : ''}
 				to='/mytrips/ownedtrips'
 			>
 				<div className={styles.link}>
 					<img
 						className={styles.icon}
-						src='../../src/assets/icons/auto-mode-dark.svg'
+						src={
+							pathname === '/mytrips/ownedtrips'
+								? '../../src/assets/icons/auto-mode-light.svg'
+								: '../../src/assets/icons/auto-mode-dark.svg'
+						}
 						alt='Ikona skrótu do Moje Podróże'
 					/>
 					<span>Moje podróże</span>
 				</div>
 			</Link>
 			<Link
-				className={`{pathname === '/userprofile' ? styles.active : ''}`}
-				to='/mytrips'
+				className={pathname === '/userprofile' ? styles.active : null}
+				to='/userprofile'
 			>
 				<div className={styles.link}>
 					<img
 						className={styles.icon}
-						src='../../src/assets/icons/account-circle-dark.svg'
+						src={
+							pathname === '/userprofile'
+								? '../../src/assets/icons/account-circle-light.svg'
+								: '../../src/assets/icons/account-circle-dark.svg'
+						}
 						alt='Ikona skrótu do Profil'
 					/>
 					<span>Profil</span>
