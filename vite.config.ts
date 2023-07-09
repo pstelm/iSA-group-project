a2f7dc3cabb0b548e9b01b378cffbda9f7702b64
ryczbo <ryczbo09@gmail.com>
2023-07-09 14:15:28 +0200

    modify vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	base: '/jfdzr11-team-promisesland/',
+	build: {
+		chunkSizeWarningLimit: 1600,
+		outDir: 'build',
+	},
+	plugins: [react()],
+});
