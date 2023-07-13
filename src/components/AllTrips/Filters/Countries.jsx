96b33921e3125deceb7919401021072d536875fe
Paulina <paulina.bistula@gmail.com>
2023-07-13 16:19:51 +0200

    Add Countries component

---
+++ src/components/AllTrips/Filters/Countries.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './Countries.module.css';
+
+const Countries = ({ countriesData, selectedCountry, setSelectedCountry }) => {
+	return (
+		<div>
+			<select
+				className={styles.country_list}
+				onChange={(e) => setSelectedCountry(e.target.value)}
+				required
+			>
+				<option defaultValue={''} hidden>
+					Wybierz kraj
+				</option>
+				{countriesData.map((country) => {
+					return (
+						<option key={country} value={country}>
+							{country}
+						</option>
+					);
+				})}
+			</select>
+		</div>
+	);
+};
+
+export default Countries;
