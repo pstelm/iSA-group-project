34ae79541c143be6d95494418469c9bd3530ebbd
Katarzyna Machaj <katarzyna.machaj.94@gmail.com>
2023-07-14 20:32:52 +0200

    Delete comments in ModalPopup component

--- src/components/ModalPopup/ModalPopup.jsx
+++ src/components/ModalPopup/ModalPopup.jsx
@@ -1,43 +1,23 @@
 import React from 'react';
 import styles from './ModalPopup.module.css';
 import Popup from 'reactjs-popup';
-// import { toast } from 'react-hot-toast';
-// import { useNavigate } from 'react-router-dom';
 
 const ModalPopup = ({
 	triggerBtn,
 	modalHeader,
 	modalAdditionalInfo,
 	handleConfirmCancelationClick,
-	// pathName,
 }) => {
-	// const navigate = useNavigate();
-
 	return (
-		<Popup
-			trigger={
-				triggerBtn
-				// <button type='button' className={styles.cancel_btn}>
-				// 	Anuluj
-				// </button>
-			}
-			modal
-			nested
-		>
+		<Popup trigger={triggerBtn} modal nested>
 			{(close) => (
 				<div className={styles.overlay}>
 					<div className={styles.modal}>
 						<button className={styles.close_sign} onClick={close}>
 							&times;
 						</button>
-						<div className={styles.modal_header}>
-							{modalHeader}
-							{/* Czy na pewno chcesz anulować edycję? */}
-						</div>
-						<p className={styles.modal_additional_info}>
-							{modalAdditionalInfo}
-							{/* Spowoduje to usunięcie wszystkich wprowadzonych danych. */}
-						</p>
+						<div className={styles.modal_header}>{modalHeader}</div>
+						<p className={styles.modal_additional_info}>{modalAdditionalInfo}</p>
 						<div className={styles.actions}>
 							<button
 								className={`${styles.actions_btn} + ${styles.actions_btn_cancel}`}
@@ -49,9 +29,6 @@ const ModalPopup = ({
 								className={`${styles.actions_btn} + ${styles.actions_btn_confirm}`}
 								onClick={() => {
 									handleConfirmCancelationClick();
-									// navigate('/userprofile');
-									// navigate(pathName);
-									// toast.error('Twoje zmiany nie zostały zapisane');
 									close();
 								}}
 							>
