e8d5b05e6c218e72cff1d63baed04e818547bd2d
Paulina <paulina.bistula@gmail.com>
2023-07-15 11:26:49 +0200

    Fix icons in Navbar and Home

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './Navbar.module.css';
 import useAuth from '../../contexts/AuthContext';
+import logo from '/public/assets/logo_transparent.png';
 
 const Navbar = () => {
 	const { logout } = useAuth();
@@ -8,19 +9,13 @@ const Navbar = () => {
 	return (
 		<div className={styles.navbar}>
 			<div className={styles.navLogo}>
-				<img src='/assets/logo_transparent.png' alt='Logo CoTravel' />
+				<img src={logo} alt='Logo CoTravel' />
 				<h1 className={styles.name}>CoTravel</h1>
 			</div>
-			{/* <button className={styles.hamburgerBtn} type="button">
-				<div className={styles.hamburgerBtnBox}>
-					<span className={styles.hamburgerBtnLine}>MENU</span>
-				</div>
-			</button> */}
+
 			<button className={styles.logoutBtn} onClick={logout} type='button'>
 				Wyloguj się
 			</button>
-
-			{/* <nav className={styles.nav}></nav> */}
 		</div>
 	);
 };
